b71e42396a2db4160f7c415a0d0673e7aa28f669
dmcneill15 <dvveijeren@gmail.com>
2024-08-08 22:35:08 +1200

    started creating pizza cards

--- src/components/PizzaCard.jsx
+++ src/components/PizzaCard.jsx
@@ -1,27 +1,48 @@
 'use client' // client component, not server rendered
 
-import { Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
+
+const pizzaData = [
+    {
+        id: 1,
+        image: 'Pizza image',
+        title: 'Three Cheese Special',
+        price: '$24.99',
+    }, //
+    {
+        id: 2,
+        image: 'Pizza image',
+        title: 'Margherita',
+        price: '$14.99',
+    },
+    {
+        id: 3,
+        image: 'Pizza image',
+        title: 'Delux',
+        price: '$21.99',
+    }
+]
 
 function PizzaCard() {
 
     return (
-        <Row xs={6} md={6} className="g-4">
-            {Array.from({ length: 4 }).map((_, idx) => (
-                <Col key={idx}>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit
-                                longer.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            ))}
+        
+        <Row xs={2} md={6} className="g-4 justify-content-center">
+                {pizzaData.map(pizza => (
+                    <Col key={pizza.id} className="">
+                        <Card className='border-light border-2 text-center justify-content-center' style={{ backgroundColor: '#eeeade' }}>
+                            <Card.Img className="mx-auto" style={{ width: '180px', height: 'auto' }} src="/images/roma.png" />
+                            <Card.Body>
+                                <Card.Title className='fs-5'>{pizza.title}</Card.Title>
+                                <Card.Text>{pizza.price}</Card.Text>
+                                <button type="button" class="btn btn-outline-danger">Add to Cart</button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                   
+                ))}
         </Row>
+        
     )
 }
 
