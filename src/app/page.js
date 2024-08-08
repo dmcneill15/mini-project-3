2ee1687458686404279299cea14212108ff95278
dmcneill15 <dvveijeren@gmail.com>
2024-08-08 20:27:05 +1200

    bootstrap installation

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import PizzaCard from "@/components/PizzaCard";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/images/ThreeCheeseLogo.png"
-              alt="Vercel Logo"
-              width={180}
-              height={180}
-              priority
-            />
-          </a>
+          <PizzaCard />
         </div>
       </div>
 
