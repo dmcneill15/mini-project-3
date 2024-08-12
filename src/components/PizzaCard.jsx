2044a606867ba05858144f9909427bb3e3c7270a
dmcneill15 <dvveijeren@gmail.com>
2024-08-12 21:49:14 +1200

    Updated pizza card display

--- src/components/PizzaCard.jsx
+++ src/components/PizzaCard.jsx
@@ -1,22 +1,24 @@
 'use client' // client component, not server rendered
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row, Container, CardFooter } from 'react-bootstrap';
 
-function PizzaCard({pizzaList}) {
+function PizzaCard({ pizzaList }) {
     return (
-        <Row xs={1} sm={2} md={4} className="g-4 justify-content-center">
-                {pizzaList.map(pizza => (
-                    <Col key={pizza.id} className=" custom-col d-flex justify-content-center">
-                        <Card className='border-light border-2 text-center' style={{ backgroundColor: '#eeeade', width:'250px' }}>
-                            <Card.Img className="mx-auto" style={{ width: '180px', height: 'auto' }} src={pizza.image} />
+        <Container className='justify-content-center align-items-center'>
+            <Row xs={1} sm={2} md={4} className="justify-content-center">
+            {pizzaList.map(pizza => (
+                    <Col key={pizza.id} className="g-4 justify-content-center">
+                        <Card className='border-light border-2 text-center' style={{ backgroundColor: '#eeeade'}}>
+                            <Card.Img className="mx-auto pt-2" style={{ width: '180px', height: 'auto' }} src={pizza.image} />
                             <Card.Body>
                                 <Card.Title className='fs-5'>{pizza.title}</Card.Title>
                                 <Card.Text>{pizza.price}</Card.Text>
-                                <Button type="submit" variant="outline-danger">Add to Cart</Button>
+                               <CardFooter style={{ backgroundColor: '#eeeade'}}> <Button type="submit" variant="outline-danger">Add to Cart</Button></CardFooter>
                             </Card.Body>
                         </Card>
                     </Col>
                 ))}
-        </Row>
+            </Row>
+        </Container >
     )
 }
 
