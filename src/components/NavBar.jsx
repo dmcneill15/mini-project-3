24673ca163372016b1335f5cf51270e2975e73f4
dmcneill15 <dvveijeren@gmail.com>
2024-08-12 22:41:18 +1200

    improved formatting of navList. to-do: refactor & implement bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,26 +4,54 @@ import { usePathname } from 'next/navigation'
 import Image from "next/image";
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+//import navList from '@/data/navList'
 
 function NavBar() {
     const path = usePathname(); // hook to check current path
+
+    const navList = [
+        {
+            id: 1,
+            title: 'Home',
+            href: '/'
+        },
+        {
+            id: 2,
+            title: 'Orders',
+            href: '/ordersDashboard'
+        },
+        {
+            id: 3,
+            title: 'About',
+            href: '/about'
+        },
+        {
+            id: 4,
+            title: 'Contact',
+            href: '/contact'
+        },
+    ]
+
     return (
         <header className="NavHeader">
-            <div className="Logo">
-                <Image src="/images/ThreeCheeseLogo.png" alt="Three Cheese Logo"
-                    width={150} height={150} priority
-                />
-            </div>
-            <nav className="NavBar">
-                <ul className="NavMain">
-                    <li><Link href="/" className={path === '/' ? 'active' : null}>Home</Link></li>
-                    <li><Link href="/ordersDashboard" className={path.startsWith('/ordersDashboard') ? 'active' : null}>Orders</Link></li>
-                    <li><Link href="/about" className={path.startsWith('/about') ? 'active' : null}>About</Link></li>
-                    <li><Link href="/contact" className={path.startsWith('/contact') ? 'active' : null}>Contact</Link></li>
+            <nav className="NavBar" style={{ justifyContent: 'space-between' }}>
+                <div className="Logo" style={{marginLeft:"2em"}}>
+                    <Image src="/images/ThreeCheeseLogo.png" alt="Three Cheese Logo"
+                        width={150} height={150} priority
+                    />
+                </div>
+                <ul className="NavMain" style={{marginRight:"12em"}}>
+                    {navList.map((item, id) => (
+                        <li key={id}>
+                            <Link href={item.href} className={path === item.href ? 'active' : null}>
+                                {item.title}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
-                <ul className="NavIcons">
-                    <li><Link href="/"><SearchIcon style={{fontSize:"30px"}} /></Link></li>
-                    <li><Link href="/cart"><ShoppingCartIcon style={{fontSize:"30px"}} /></Link></li>
+                <ul className="NavIcons" style={{marginRight:"2em"}}>
+                    <li><Link href="/"><SearchIcon style={{ fontSize: "30px" }} /></Link></li>
+                    <li><Link href="/cart"><ShoppingCartIcon style={{ fontSize: "30px" }} /></Link></li>
                 </ul>
             </nav>
         </header>
---
+++ src/data/navList.js
@@ -0,0 +1,24 @@
+const navList = [
+    {
+        id: 1,
+        title: 'Home',
+        href: '/'
+    },
+    {
+        id: 2,
+        title: 'Orders',
+        href:'/ordersDashboard'
+    },
+    {
+        id: 3,
+        title: 'About',
+        href: '/about'
+    },
+    {
+        id: 4,
+        title: 'Contact',
+        href: '/contact'
+    },
+]
+
+export default navList;
